2a23c91ac238912ecebc9f7c4c8a733b7bcbc084
Mauro2028 <mauriciolizcano25531@gmail.com>
2021-02-01 00:01:53 +0000

    first commit

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Jumbotron = () => (
+	<div className="jumbotron">
+		<h1 className="display-4">A Warm Welcome!</h1>
+		<p className="lead">
+			Lorem Ipsum is simply dummy text of the printing and typesetting
+			industry. Lorem Ipsum has been the industry standard dummy text ever
+			since the 1500s
+		</p>
+		<a className="btn btn-primary btn-lg" href="#" role="button">
+			Call to action!
+		</a>
+	</div>
+);
